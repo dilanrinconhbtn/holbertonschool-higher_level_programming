cc328154a19b3bc005413b910d74a0bc7dc5ccd6
dilanrinconhbtn <937@holbertonschool.com>
2019-09-10 20:19:53 +0000

    5 y 6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for first_digit in range(0, 10):
-    for second_digit in range(0,10):
-        print("{}{}".format(first_digit, second_digit), end='')
-        if first_digit != 9 or second_digit != 9:
-            print(", ", end='')
+for i in range(0, 99):
+        print("{:02d}, ".format(i), end='')
+print ("99")
