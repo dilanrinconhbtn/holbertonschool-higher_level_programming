c5fdb63e0b8aae97cad9510744b9658621f212ca
dilanrinconhbtn <937@holbertonschool.com>
2019-09-10 20:22:10 +0000

    5 y 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for first_digit in range(0, 10):
-    for second_digit in range(0,10):
+    for second_digit in range(0, 10):
         if first_digit < second_digit:
-            if first_digit !=  8 or second_digit != 9:
+            if first_digit != 8 or second_digit != 9:
                 print("{}{}, ".format(first_digit, second_digit), end='')
             else:
                 print("89")
