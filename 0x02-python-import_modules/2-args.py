3b949de4e5537070c11fb616df475702695a81f2
dilanrinconhbtn <937@holbertonschool.com>
2019-09-13 02:27:08 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    if len(sys.argv) == 2:
+        print("1 argument:")
+    if len(sys.argv) > 2:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(len(sys.argv)):
+        if i > 0:
+            print("{}: {}".format(i, sys.argv[i]))
+            i = i + 1
