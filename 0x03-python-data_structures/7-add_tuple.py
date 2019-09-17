a3a3bae87305a3b5f4c7cef574c1dd9f2ceca844
dilanrinconhbtn <937@holbertonschool.com>
2019-09-17 21:49:23 +0000

    adding in tupple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    sizea = len(tuple_a)
-    sizeb = len(tuple_b)
-    for i in range(sizea):
-        if sizea < 2:
-            tuple_a += (0,)
-        elif sizeb < 2:
-            tuple_b += (0,)
-    c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return c
+   tuple_b += (0, 0)
+   tuple_a += (0, 0)
+   c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+   return c
