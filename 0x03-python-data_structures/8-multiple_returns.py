1c4aaff3980a65b92bd39891279a34f030af0832
dilanrinconhbtn <937@holbertonschool.com>
2019-09-17 23:56:56 +0000

    fix condition

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return len(sentence), sentence[0]
+    if sentence:
+        t = (len(sentence), sentence[0])
+    else:
+        t = (0, none)
+    return t
