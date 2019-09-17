4e93f0af679dd10f829dac19483e0147b8ef26b7
dilanrinconhbtn <937@holbertonschool.com>
2019-09-17 23:20:29 +0000

    fix condition

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,4 +3,6 @@ def new_in_list(my_list, idx, element):
     new = my_list.copy()
     if idx >= 0 and idx < (len(new)):
         new[idx] = element
+    else:
+        return my_list
     return new
