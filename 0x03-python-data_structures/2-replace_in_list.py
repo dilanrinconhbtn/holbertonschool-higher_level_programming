0388df43fae4e6ceec9160c48f09496f64d59f03
dilanrinconhbtn <937@holbertonschool.com>
2019-09-17 17:36:20 +0000

    replace

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,5 +2,8 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return None
+    elif len(my_list) == 0:
+        return None
     else:
+        my_list[idx] = element
         return my_list
