3b240438f5a4378ba1f96d4a77f1bfeb45eb5763
dilanrinconhbtn <937@holbertonschool.com>
2019-09-19 19:36:57 +0000

    updating

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     new = my_list.copy()
     for i in range(len(new)):
-        if new[i] is 2:
+        if new[i] == search:
             new.pop(i)
-            new.insert(i, 89)
+            new.insert(i , replace)
     return new
