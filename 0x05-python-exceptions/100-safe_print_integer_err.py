0d630346e700896dfa87e4a09e3391ce6118e513
dilanrinconhbtn <937@holbertonschool.com>
2019-09-30 22:11:17 +0000

    safe print error

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,7 @@
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as error:
+        print("Exception: {}".format(error))
+        return False
