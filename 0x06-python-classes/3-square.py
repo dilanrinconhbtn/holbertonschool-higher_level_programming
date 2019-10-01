332adeb6aae23558273d3ab362e49a11bfcb88e2
dilanrinconhbtn <937@holbertonschool.com>
2019-10-01 19:14:12 +0000

    area of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        """__init__ method.
+        Args:
+        param1 (str): Description of `param1`.
+        param2 (:obj:`int`, optional): Description of `param2`. Multiple
+        lines are supported.
+        param3 (:obj:`list` of :obj:`str`): Description of `param3`.
+        size (int): size of the square
+        """
+        if isinstance(size, int) and size >= 0:
+            self.__size = size
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """area method"""
+        return self.__size * self.__size
