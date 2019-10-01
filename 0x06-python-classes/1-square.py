dbfe99d204c4b24f431f42dfeb79f8ed13188013
dilanrinconhbtn <937@holbertonschool.com>
2019-10-01 18:40:53 +0000

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square"""
+    def __init__(self, size):
+        """__init__ method.
+        Args:
+        param1 (str): Description of `param1`.
+        param2 (:obj:`int`, optional): Description of `param2`. Multiple
+        lines are supported.
+        param3 (:obj:`list` of :obj:`str`): Description of `param3`.
+        size (int): size of the square
+        """
+        self.__size = size
