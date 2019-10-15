9c74db20df0ffffdfc9ac1c466c07a8825198ae0
dilanrinconhbtn <937@holbertonschool.com>
2019-10-15 17:51:33 +0000

    My list sorted

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,14 @@
-def print_sorted(self):
-    """ Print sorted"""
-    for i in range(self.my_list):
-        if self.my_list[i] < self.my_list[i + 1]:
-            
+#!/usr/bin/python3
+"""
+Class MyList
+"""
+class MyList(list):
+    """
+    Args:
+        * l_sorted: Variable for stored the sorted list
+        * self: variable that refers to the object itself
+    """
+    def print_sorted(self):
+        l_sorted = self.copy()
+        l_sorted.sort()
+        print(l_sorted)
