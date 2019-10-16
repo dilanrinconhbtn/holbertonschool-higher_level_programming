183f7b2bb688e485786ef31ebef1f11458bbb597
dilanrinconhbtn <937@holbertonschool.com>
2019-10-16 23:51:21 +0000

    task 9

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import os
+import sys
+save = __import__('7-save_to_json_file').save_to_json_file
+load = __import__('8-load_from_json_file').load_from_json_file
+
+
+if os.path.exists('add_item.json') is True:
+    lis = list(load("add_item.json"))
+    for i in range(1, len(sys.argv)):
+        lis.append(sys.argv[i])
+    save(lis, "add_item.json")
+else:
+    lis = []
+    save(lis, "add_item.json")
