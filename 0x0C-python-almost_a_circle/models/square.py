67b248ceb12a512d3a1a570fd1e40b45b7b397dd
dilanrinconhbtn <937@holbertonschool.com>
2019-10-23 01:16:37 +0000

    correction pep8 line 14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,7 +11,8 @@ class Square(Rectangle):
 
     def __str__(self):
         """return string"""
-        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width))
+        return ("[Square] ({}) {}/{} - {}".format(self.id,
+                                                  self.x, self.y, self.width))
 
     @property
     def size(self):
