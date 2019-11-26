557970512ede7a16cffff074769b302a6fadcbb2
dilanrinconhbtn <937@holbertonschool.com>
2019-11-26 19:22:16 +0000

    not null auto incremente new database and new table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Create data base, table, id unique, autogenerate and primarykey
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL);
