473a1cf0179ae7695dda9af9bcbbe038f0693567
dilanrinconhbtn <937@holbertonschool.com>
2019-11-26 18:01:27 +0000

    creating user and priviliges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Create user and give all privileges, password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
