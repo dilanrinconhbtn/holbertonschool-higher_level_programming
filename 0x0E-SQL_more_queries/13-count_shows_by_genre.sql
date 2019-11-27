2aa45da33641f9905b842c1b07693aec98aac9e7
dilanrinconhbtn <937@holbertonschool.com>
2019-11-27 03:11:24 +0000

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,6 @@
---  script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id ORDER BY number_of_shows DESC, tv_genres.id ASC;
+--  script that lists all genres
+SELECT tv_genres.name AS genre, COUNT(tv_shows.title) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
