de87da778c1af9f68d1fdccc678b82e9158ce519
dilanrinconhbtn <937@holbertonschool.com>
2019-12-14 16:49:01 +0000

    adding state

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+N_session = Session()
+
+
+N_user = State(name="Louisiana")
+N_session.add(N_user)
+N_session.commit()
+N_session.close()
