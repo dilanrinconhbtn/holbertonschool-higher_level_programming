69340e56c7108fabaec602c6c68a0e1a09006dfa
dilanrinconhbtn <937@holbertonschool.com>
2019-12-15 17:05:10 +0000

    fix

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -11,12 +11,10 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     N_session = Session()
-
     N_user = State(name='Louisiana')
     N_session.add(N_user)
     N_session.commit()
-    print(new_state.id)
+    print(N_user.id)
     N_session.close()
