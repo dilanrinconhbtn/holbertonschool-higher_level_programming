1a71be5b44c3343b5f264dcf35f7c6b58d44acda
dilanrinconhbtn <937@holbertonschool.com>
2019-12-16 02:32:23 +0000

    fixing

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""
+a script that lists all State objects from the database
 """
 import sys
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
+from model_city import City
 from sqlalchemy import (create_engine)
-
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    N_session = Session()
+    new_session = Session()
 
-    for class_instance in N_session.query(State, City).filter(State.id ==
+    for class_instance in new_session.query(State, City).filter(State.id ==
                                                                 City.state_id):
         print("{}: ({}) {}".format(class_instance.State.name,
                                    class_instance.City.id,
                                    class_instance.City.name))
-    N_session.close()
+
+    new_session.close()
