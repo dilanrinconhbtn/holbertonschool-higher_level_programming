19fd11f14ca18602deda17b96131a76cf39865ea
dilanrinconhbtn <937@holbertonschool.com>
2019-12-16 02:26:53 +0000

    correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 
+import MySQLdb
+from sys import argv
 
 """
-a script that lists all states
+Lists all states with a name starting with N
 """
 if __name__ == "__main__":
 
@@ -12,9 +12,8 @@ if __name__ == "__main__":
                          db=argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states
-    WHERE name LIKE BINARY'N%'\
-    ORDER BY states.id""")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY'N%'\
+                ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
