99a1654bc6299b86ce362ff5e31ad6baf83730ff
dilanrinconhbtn <937@holbertonschool.com>
2020-02-04 12:41:24 -0500

    adding missing size

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const argu = process.argv[2];
 const myword = 'X';
-for (let j = 0; j < argu; j++) {
-  console.log(myword.repeat(argu));
+if (process.argv.length > 2) {
+  for (let j = 0; j < argu; j++) {
+    console.log(myword.repeat(argu));
+  }
+} else {
+  console.log('Missing size');
 }
