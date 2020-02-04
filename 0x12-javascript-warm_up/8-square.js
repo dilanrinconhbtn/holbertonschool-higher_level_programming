34a883ad53d484860dbf4afe3da20c4ff8fb63a3
dilanrinconhbtn <937@holbertonschool.com>
2020-02-04 12:37:43 -0500

    fix semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const argu = process.argv[2];
-const myword = 'X'
+const myword = 'X';
 for (let j = 0; j < argu; j++) {
-    console.log(myword.repeat(argu));
+  console.log(myword.repeat(argu));
 }
