82581ffbf48bcc004c5948ef77b36b03a5d93981
dilanrinconhbtn <937@holbertonschool.com>
2020-02-10 15:20:36 -0500

    esreverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-exports.esrever = function (list){
-    let n_list = [];
-    for (let index = list.length -1 ; index >= 0; index--) {
-        n_list.push(list[index]);
-    }
-    return n_list;
-}
+exports.esrever = function (list) {
+  const n_list = [];
+  for (let index = list.length - 1; index >= 0; index--) {
+    n_list.push(list[index]);
+  }
+  return n_list;
+};
