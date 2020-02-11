213b3c24ac710b078544801d4604bcd012723074
dilanrinconhbtn <937@holbertonschool.com>
2020-02-11 10:43:52 -0500

    status

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (err, response) {
+request.get(process.argv[2], function (err, response) {
   if (err) {
     console.error(err);
   } else {
