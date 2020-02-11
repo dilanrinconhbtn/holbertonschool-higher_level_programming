87f1450eb8ae5f88f6d4ee10cf8d281716773be2
dilanrinconhbtn <937@holbertonschool.com>
2020-02-11 13:26:00 -0500

    starwars again

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const character = 'https://swapi.co/api/people/18/';
 let count = 0;
 request(url, function (err, response, body) {
   if (err) {
     console.error(err);
   } else {
-    const obj = JSON.parse(body);
-    for (const x in obj.results) {
-      for (const y in obj.results[x].characters) {
-        if (obj.results[x].characters[y] === character) {
+    const obj = JSON.parse(body).results;
+    for (const film of obj) {
+      for (const charac of film.characters) {
+        if (charac.search('/18/') > 0) {
           count++;
         }
       }
     }
-    console.log(count);
   }
+  console.log(count);
 });
