347e073d7384c9efe0791c2ca463b9278cbae405
dilanrinconhbtn <937@holbertonschool.com>
2020-02-10 20:21:10 -0500

    map function

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const list = require('./100-data').list;
+const map1 = list.map(function (currentValue, index) {
+  return currentValue * index;
+});
+console.log(list);
+console.log(map1);
